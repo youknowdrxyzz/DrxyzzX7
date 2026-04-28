68b8bc7d41845884235c0966cdc8bd4259cf213c
youknowdrxyzz <youknowdrxyzz@gmail.com>
2026-04-29 03:56:29 +0700

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -79,9 +79,9 @@ const PAUSE_DURATION = 2000;
 const WORDS = ["Network & Telecom Student", "Tech Enthusiast"];
 const TECH_STACK = ["React", "Javascript", "Node.js", "Tailwind"];
 const SOCIAL_LINKS = [
-  { icon: Github, link: "https://github.com/EkiZR", label: "GitHub Profile" },
-  { icon: Linkedin, link: "https://www.linkedin.com/in/ekizr/", label: "LinkedIn Profile" },
-  { icon: Instagram, link: "https://www.instagram.com/ekizr_/?hl=id", label: "Instagram Profile" }
+  { icon: Github, link: "https://github.com/youknowdrxyzz", label: "GitHub Profile" },
+  { icon: Linkedin, link: "https://www.linkedin.com/in/drxyzzx/", label: "LinkedIn Profile" },
+  { icon: Instagram, link: "https://www.instagram.com/youknowdrxyzz", label: "Instagram Profile" }
 ];
 
 const Home = () => {
@@ -140,25 +140,25 @@ const Home = () => {
   return (
     <>
       <Helmet>
-        <title>Eki Zulfar Rachman — Frontend Web Developer</title>
-        <meta name="description" content="Website resmi Eki Zulfar Rachman, Front-End Web Developer. Saya berfokus pada penciptaan pengalaman digital yang menarik dan selalu berupaya memberikan solusi terbaik dalam setiap proyek yang saya kerjakan." />
+        <title>DrxyzzX7— Frontend Web Developer</title>
+        <meta name="description" content="Website resmi DrxyzzX7, Front-End Web Developer. Saya berfokus pada penciptaan pengalaman digital yang menarik dan selalu berupaya memberikan solusi terbaik dalam setiap proyek yang saya kerjakan." />
      <meta name="robots" content="index, follow" />
-        <link rel="canonical" href="https://ekizr.com" />
-        <meta property="og:title" content="Eki Zulfar Rachman — Frontend Web Developer" />
-     <meta property="og:description" content="Website resmi dan portofolio Eki Zulfar Rachman, Front-End Web Developer." />
-        <meta property="og:url" content="https://ekizr.com" />
+        <link rel="canonical" href="https://drxyzzx.vercel.app" />
+        <meta property="og:title" content="DrxyzzX7 — Frontend Web Developer" />
+     <meta property="og:description" content="Website resmi dan portofolio DrxyzzX7, Front-End Web Developer." />
+        <meta property="og:url" content="https://drxyzzx.vercel.app" />
         <meta property="og:type" content="website" />
         <script type="application/ld+json">{`
           {
             "@context": "https://schema.org",
             "@type": "Person",
-            "name": "Eki Zulfar Rachman",
+            "name": "DrxyzzX7",
             "jobTitle": "Frontend Developer",
-            "url": "https://ekizr.com",
+            "url": "https://drxyzzx.vercel.app",
             "sameAs": [
-              "https://github.com/EkiZR",
-              "https://www.linkedin.com/in/ekizr/",
-              "https://www.instagram.com/ekizr_/"
+              "https://github.com/youknowdrxyzz",
+              "https://www.linkedin.com/in/drxyzzx/",
+              "https://www.instagram.com/youknowdrxyzz/"
             ]
           }
         `}</script>
