30e4d307df87cf7c313ace0d0f980deb8c1d9335
youknowdrxyzz <youknowdrxyzz@gmail.com>
2026-04-29 03:51:31 +0700

    Update Contact.jsx

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -45,7 +45,7 @@ const ContactPage = () => {
 
     try {
       // Ganti dengan email Anda di FormSubmit
-      const formSubmitUrl = 'https://formsubmit.co/ekizulfarrachman@gmail.com';
+      const formSubmitUrl = 'https://formsubmit.co/drxyzzx@gmail.com';
       
       // Siapkan data form untuk FormSubmit
       const submitData = new FormData();
