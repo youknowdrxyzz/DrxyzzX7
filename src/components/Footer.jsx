6744d1a3a709aa4ab690e40fe0b205b0434e20c9
youknowdrxyzz <youknowdrxyzz@gmail.com>
2026-04-29 05:21:23 +0700

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,8 +7,8 @@ const Footer = () => {
         <hr className="my-3 border-gray-400 opacity-15 sm:mx-auto lg:my-6 text-center" />
         <span className="block text-sm pb-4 text-gray-500 text-center dark:text-gray-400">
           © {currentYear}{" "}
-          <a href="https://ekizr.com" className="hover:underline">
-            EkiZR™
+          <a href="https://drxyzzx.vercel.app" className="hover:underline">
+            DrcuzzX7™
           </a>
           . All Rights Reserved.
         </span>
