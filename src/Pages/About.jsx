5a1a035e97f8f77d0bc828a8d629b89e0bc1ebe8
youknowdrxyzz <youknowdrxyzz@gmail.com>
2026-04-29 03:50:08 +0700

    Update About.jsx

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -209,7 +209,7 @@ const AboutPage = () => {
                 data-aos-duration="1300"
                 itemProp="name"
               >
-                Eki Zulfar Rachman
+                DrxyzzX7
               </span>
             </h2>
             
